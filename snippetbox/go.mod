639b1f01a77c97906562b7f2977ed406fc39b6a4
user <user@usernoMacBook-Pro.local>
2024-05-02 00:05:19 +0530

    first go commit

---
+++ snippetbox/go.mod
@@ -0,0 +1,3 @@
+module snippetbox.jagdish.net
+
+go 1.21.3
---
+++ snippetbox/main.go
@@ -0,0 +1,35 @@
+package main
+
+
+import (
+    "log"
+    "net/http"
+)
+
+func home(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("hello I am root"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("I am a snippet"))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("create snippet"))
+}
+
+func main(){
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet/view",snippetView)
+
+	mux.HandleFunc("/snippet/create",snippetCreate)
+
+
+	log.Print("server started at 4000")
+
+	err := http.ListenAndServe(":4000",mux)
+
+	log.Fatal(err)
+}
