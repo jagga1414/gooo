b4dab8f9b13547896ad66a719e598e7e8b69c75b
jagga1414 <surajsamant1414@gmail.com>
2024-06-09 00:04:42 +0530

    view page changes added

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -11,9 +11,9 @@ import (
 )
 
 type snippetCreateForm struct {
-	Title               string `form:"title"`
-	Content             string `form:"content"`
-	FileName			string `form:"filename"`
+	Title    string `form:"title"`
+	Content  string `form:"content"`
+	FileName string `form:"filename"`
 	// Expires             int    `form:"expires"`
 	validator.Validator `form:"-"`
 }
@@ -219,7 +219,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.NotBlank(form.FileName),"filename", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.FileName), "filename", "This field cannot be blank")
 	// form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 	// Use the Valid() method to see if any of the checks failed. If they did, // then re-render the template passing in the form in the same way as
 	// before.
